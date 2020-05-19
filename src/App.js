9de8956302ac105b60835b54251a29ebf2517d61
shafali03 <shaf03sm@gmail.com>
2020-05-19 18:00:45 +0100

    feat: Passing the props through as a parameter and map though the array to return a table row for each object in the array

--- src/App.js
+++ src/App.js
@@ -3,9 +3,29 @@ import Table from './Table'
 
 class App extends Component {
   render() {
+
+    const characters = [
+      {
+        name: 'Sarah',
+        job: 'UX',
+      },
+      {
+        name: 'Charlie',
+        job: 'UI',
+      },
+      {
+        name: 'Lee',
+        job: 'Actor',
+      },
+      {
+        name: 'Dennis',
+        job: 'Barber',
+      },
+    ]
+
     return (
       <div className="container">
-        <Table />
+        <Table characterData={characters} />
       </div>
     )
   }
